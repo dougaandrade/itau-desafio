8617a0f19e2a3199cca34c98d9319b974647affd
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2026-01-22 12:26:19 -0300

    add EstatisticaController with REST endpoint

---
+++ src/main/java/com/itau/itau/controller/EstatisticaController.java
@@ -0,0 +1,10 @@
+package com.itau.itau.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estatistica")
+public class EstatisticaController {
+
+}
