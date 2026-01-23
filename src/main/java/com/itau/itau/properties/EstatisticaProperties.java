eb9a4b9ec2026f53a01fa29bc6e5845eb1122049
Douglas Andrade <douglas.andrade@tre-se.jus.br>
2026-01-23 12:51:16 -0300

    update dependencies in pom.xml and add validation annotations to EstatisticaProperties and TransacaoRequest

--- src/main/java/com/itau/itau/properties/EstatisticaProperties.java
+++ src/main/java/com/itau/itau/properties/EstatisticaProperties.java
@@ -1,8 +1,12 @@
 package com.itau.itau.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Validated
 @ConfigurationProperties(prefix = "estatistica")
-public record EstatisticaProperties(Integer intervaloEmSegundos) {
-
+public record EstatisticaProperties(@NotNull @Positive Integer intervaloEmSegundos) {
 }
--- src/main/java/com/itau/itau/request/TransacaoRequest.java
+++ src/main/java/com/itau/itau/request/TransacaoRequest.java
@@ -3,6 +3,8 @@ package com.itau.itau.request;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TransacaoRequest {
 
+  @NotNull(message = "O valor da transacao nao pode ser nulo")
+  @Positive(message = "O valor da transacao deve ser maior que zero")
   private BigDecimal valor;
   private OffsetDateTime dataHora;
 }
